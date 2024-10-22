b3ec7482613a3debe4a0f9f1bc9c951a22a9bb87
Nohadon <pol.gonzalo@optickssecurity.com>
2024-10-22 18:31:40 +0200

    Main Menu responsive SEMIDONE

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,27 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { EventInfoWrapper } from '@angular/core/primitives/event-dispatch';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+class Menu {
+
+  private navMenu: HTMLDivElement;
+  private clicked: Boolean = false;
+
+  constructor() {
+    let btn = <HTMLInputElement>document.getElementById("display_menu_btn");
+    btn.addEventListener("click", (_: Event) => this.onDisplayMenu());
+    this.navMenu = <HTMLDivElement>document.getElementById("main_menu_navigation")
+
+  }
+
+  onDisplayMenu() {
+    this.navMenu.classList.toggle("show");
+  }
+}
+
+new Menu();
+

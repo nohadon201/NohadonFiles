a2423db78f2803ce6c07fc77dd4b973c86959975
Nohadon <pol.gonzalo@optickssecurity.com>
2024-10-28 17:56:28 +0100

    re-ordered some sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { DevelopmentProjectsComponent } from './development-projects/development-projects.component';
-import { NohadengineComponent } from './nohadengine/nohadengine.component';
-import { GraphicsDevelopmentComponent } from './graphics-development/graphics-development.component';
-import { AboutMeComponent } from './about-me/about-me.component';
+import { HomeComponent } from './main-sections/home/home.component';
+import { DevelopmentProjectsComponent } from './main-sections/development-projects/development-projects.component';
+import { NohadengineComponent } from './main-sections/nohadengine/nohadengine.component';
+import { GraphicsDevelopmentComponent } from './main-sections/graphics-development/graphics-development.component';
+import { AboutMeComponent } from './main-sections/about-me/about-me.component';
 
 
 @NgModule({
